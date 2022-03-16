1009c4b5c302df8371dbecb9767996a1cd91646c
ProteinK <konprotein@gmail.com>
2022-03-16 14:27:05 +0100

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let result = arr;
 
+  for (let i = 0; i < args.length; i++) {
+    const index = result.findIndex(e => e === args[i]);
+    if (index !== -1) {
+      result.splice(index, 1);
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
