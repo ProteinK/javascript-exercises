6f691076e95eccc67e247ee58277d67066ac778f
ProteinK <konprotein@gmail.com>
2022-03-16 14:19:09 +0100

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  let result = text.split('');
+  result.reverse();
+  result = result.join('');
 
+  return result;
 };
 
 // Do not edit below this line
