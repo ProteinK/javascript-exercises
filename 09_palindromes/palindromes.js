780f677595b514e94e4f60636a24baad9dfcfafa
ProteinK <konprotein@gmail.com>
2022-03-24 13:09:22 +0100

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[!,.\s]/g, '');
+  str = str.toLowerCase();
+  let reversed = str.split('').reverse().join('');
+  return str === reversed;
 };
 
 // Do not edit below this line
