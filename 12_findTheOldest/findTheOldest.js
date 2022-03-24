659b53a0d45147f647dbb0f8c96dc8ee45233d2a
ProteinK <konprotein@gmail.com>
2022-03-24 13:28:20 +0100

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, p) => {
+    let oldAge = (oldest.yearOfDeath || 2022) - oldest.yearOfBirth;
+    let age = (p.yearOfDeath || 2022) - p.yearOfBirth;
+    if (age > oldAge) {
+      oldest = p;
+    }
+    return oldest;
+  }, arr[0]);
 };
 
 // Do not edit below this line
