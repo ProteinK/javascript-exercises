ac6c4c0c24ca63a1985a6f9e0d68236834b986d2
ProteinK <konprotein@gmail.com>
2022-03-24 13:18:58 +0100

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 1) {
+    return 'OOPS';
+  }
 
+  if (typeof n === 'string') {
+    n = Number(n);
+  }
+
+  let fib = [1, 1];
+
+  let i = 0;
+  while (fib.length < n) {
+    let a = fib[i];
+    let b = fib[i + 1];
+    fib.push(a + b);
+    i++;
+  }
+
+  return fib[n - 1];
 };
 
 // Do not edit below this line
