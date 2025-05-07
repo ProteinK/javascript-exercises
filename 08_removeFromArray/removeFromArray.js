216d8218ae47b326e81b5dd1d4ce56493adaeaab
ProteinK <konprotein@gmail.com>
2025-05-07 22:30:23 +0200

    Solve 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+  const result = [...arr];
+  for (num of nums) {
+    let index = result.indexOf(num);
+    while (index !== -1) {
+      result.splice(index, 1);
+      index = result.indexOf(num);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
