20e1b55de424baaf46763804474f86e9ecb6e2ee
ProteinK <konprotein@gmail.com>
2025-05-07 22:22:32 +0200

    Solve 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let result = string.split('');
+  result = result.toReversed();
+  result = result.join('');
+  return result;
 };
 
 // Do not edit below this line
