6702a3324de1470cd4a01a7528a62c528ac05a0c
ProteinK <konprotein@gmail.com>
2025-05-12 18:19:52 +0200

    Solve 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let toCompare = str.replace(/\W/gm, '');
+  let result = toCompare.split('').reverse().join('');
+  return toCompare.toLowerCase() === result.toLowerCase();
 };
 
 // Do not edit below this line
