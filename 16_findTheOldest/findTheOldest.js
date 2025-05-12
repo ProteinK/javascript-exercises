92345581b478cb98459d7b791ebe77896177f1e0
ProteinK <konprotein@gmail.com>
2025-05-12 18:32:03 +0200

    Solve 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const sorted = arr.toSorted((a, b) => {
+    const currentYear = new Date().getFullYear();
+    let ageA = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+    let ageB = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+    return ageB - ageA;
+  });
 
+  return sorted[0];
 };
 
 // Do not edit below this line
