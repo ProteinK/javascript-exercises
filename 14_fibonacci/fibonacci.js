6e4a477232000a7255a26344dadb26949d1b4ca2
ProteinK <konprotein@gmail.com>
2025-05-12 18:25:04 +0200

    Solve 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = parseInt(n);
+  if (n < 0) return 'OOPS';
+  if (n === 0) return 0;
+  if (n < 3) return 1;
 
+  return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
 // Do not edit below this line
